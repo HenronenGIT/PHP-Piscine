1326b664a5c66a6814aca6763d65613e4e35389b
Henri <henrimaronen@gmail.com>
2022-04-20 16:57:57 +0300

    basic structure ready

--- day06/ex00/Color.class.php
+++ day06/ex00/Color.class.php
@@ -1,7 +1,7 @@
 <?php
 
 class Color {
-    public $verbose = False;
+    public static $verbose = False;
     public $red = 0;
     public $green = 0;
     public $blue = 0;
@@ -10,28 +10,31 @@ class Color {
     {
         if ($verbose == TRUE)
         {
-
+            print('constructed\n');
         }
         else
-            return ;
-
+        return ;
+        
     }
-
-    function    add()
+    
+    function    add($color)
     {
+
+        
+        return $new_color;
         // The Class must have a method called add that allows you to add
         // the the components of the current instance to the components of
         // another instance argument. The resulting color is a new
         // instance.
     }
-
+    
     function    sub()
     {
         // The Class must have a method called sub that allows you to
         // subtract the components of another instance from the components
         // of the current instance. The resulting color is a new instance.
     }
-
+    
     function    mult()
     {
         // The Class must propose a method called mult that allows you to
@@ -39,29 +42,31 @@ class Color {
         // components of of another instance argument. The resulting color
         // is a new instance.
     }
-
+    
     function    __toString()
     {
-
+        // Might need getatt()
+        return 'Color( red: ' . $this->$red . 'green: ' . $this->$green . 'blue: ' . $this->$blue . ')';
     }
-
-    function    doc()
+    
+    static function    doc()
     {
-//         The Class must have a static method called doc that returns the
-// documentation of the class in a string. (in this specific case
-// the documentation does not have to be long). The content of the
-// documentation must be read from a Color.doc.txt file. See
-// example output for formatting the documentation and content of
-// the file.
-
+        //         The Class must have a static method called doc that returns the
+        // documentation of the class in a string. (in this specific case
+        // the documentation does not have to be long). The content of the
+        // documentation must be read from a Color.doc.txt file. See
+        // example output for formatting the documentation and content of
+        // the file.
+        
         // $str = 
         // return ($str);
     }
-
+    
     function    __destruct()
     {
         if ($verbose == TRUE)
         {
+            print('destructed\n');
 
         }
         else
