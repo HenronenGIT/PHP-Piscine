40f82dcc49471864ddb01029d8c3adc2a3058151
Henri <henrimaronen@gmail.com>
2022-04-23 21:05:28 +0300

    00, 01, 02

--- day09/ex02/todo.js
+++ day09/ex02/todo.js
@@ -1,19 +1,47 @@
-
-
-// var time = new Date();
-// console.log(new Date());
-// console.log(Date.now());
-function checkCookie()
+function new_task()
 {
-    let username = getCookie("username");
-    if (username != "")
-        alert("Welcome again " + username);
-    else
+    var task = prompt('Add new task');
+    task = task.trim();
+    if (!task)
+        return ;
+    var time = Date.now();
+    div = new_div(time);
+    insert_div(task, div);
+    setCookie(time, task, 30);
+}
+
+function new_div(time)
+{
+    var div = document.createElement("div");
+    div.setAttribute("id", "task");
+    div.setAttribute("onclick", "rm_task(this, " + time + ")");
+    return (div);
+}
+
+function insert_div(task, div)
+{
+    var ft_list = document.getElementById('ft_list');
+    var task_text = document.createTextNode(task);
+    div.appendChild(task_text);
+    ft_list.insertBefore(div, ft_list.firstChild);
+    return ;
+}
+
+function rm_task(div, time_id)
+{
+    if (confirm("Are you sure you want to remove task?"))
     {
-        setCookie("username", username, 365);
-        //if (username != "" && username != null)
-        //username = prompt("Please enter your name:", "");
+        div.remove();
+        rmCookie(time_id);
     }
+    return ;
+}
+
+// Cookie functions
+function rmCookie(time_id)
+{
+    var expires = "expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    document.cookie = time_id + "=;" + expires + ":path=/";
 }
 
 function setCookie(cname, cvalue, exdays)
@@ -24,67 +52,20 @@ function setCookie(cname, cvalue, exdays)
     document.cookie = cname + "=" + cvalue + ";" + expires + ":path=/";
 }
 
-function getCookie(cname)
+function getCookie()
 {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let carr = decodedCookie.split(';');
+    let cookies = document.cookie;
+    if (!cookies)
+        return;
+    let carr = cookies.split(';');
     let i = 0;
-
     while (i < carr.length)
     {
-        let c = carr[i];
-        while (c.charAt(0) == ' ')
-            c = c.substring(1);
-        if (c.indexOf(name) == 0)
-          return c.substring(name.length, c.length);
+        let cookie = carr[i];
+        cookie_value = cookie.split('=');
+        div = new_div(cookie_value[0]);   
+        insert_div(cookie_value[1], div);
         i++
     }
-        return "";
-}
-
-function new_div()
-{
-    var new_task = document.createElement("div");
-    new_task.setAttribute("id", "task");
-    new_task.setAttribute("onclick", "rm_task(this)");
-    return (new_task);
-}
-
-function new_task()
-{
-    // Get whole ft_list
-    var ft_list = document.getElementById('ft_list');
-    // Ask task from the user if empty return
-    var task = prompt('Add new task');
-    if (!task)
-        return ;
-    // Create new <div> element
-    div = new_div();
-    //
-    /*var new_task = document.createElement("div");
-    // Add attributes to that div
-    new_task.setAttribute("id", "task");
-    new_task.setAttribute("onclick", "rm_task(this)"); */
-    //
-    // Add asked `task` to text field?
-    var task_text = document.createTextNode(task);
-    //console.log(task_text);
-    // Append text to <div> field
-    div.appendChild(task_text);
-    // Add <div> to ft_list <div>
-    ft_list.insertBefore(div, ft_list.firstChild);
-    
-    setCookie('tasks',task_text, 30);
-    // Debug
-    console.log("Debug");
-    console.log(ft_list);
-    console.log(new_task);
-}
-
-function rm_task(task)
-{
-    if (confirm("Are you sure you want to remove task?"))
-        task.remove();
     return ;
 }
