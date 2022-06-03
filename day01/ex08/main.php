734cf37542597f0349c0736ef35496357e8ce89b
Henri <henrimaronen@gmail.com>
2022-06-03 09:44:15 +0300

    tabs added instead of spaces

--- day01/ex08/main.php
+++ day01/ex08/main.php
@@ -5,7 +5,7 @@ $tab = array("!/@#;^", "42", "Hello World", "hi", "zZzZzZz");
 $tab[] = "What are we doing now ?";
 
 if (ft_is_sort($tab))
-    echo "The array is sorted\n";
+	echo "The array is sorted\n";
 else
-    echo "The array is not sorted\n";
+	echo "The array is not sorted\n";
 ?>
