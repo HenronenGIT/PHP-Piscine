b417704e464769bf982e6eb482c7438092937c47
Henri <henrimaronen@gmail.com>
2022-06-03 13:01:30 +0300

    ex10 done

---
+++ day01/ex10/do_op.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+
+if ($argc == 4)
+{
+	$i = 0;
+	while ($argv[$i])
+	{
+		$argv[$i] = trim($argv[$i]);
+		$i++;
+	}
+	$nb1 = $argv[1];
+	$sign = $argv[2];
+	$nb2 = $argv[3];
+
+	if ($sign == '+')
+		print($nb1 + $nb2 . "\n");
+	else if ($sign == '-')
+		print($nb1 - $nb2 . "\n");
+	else if ($sign == '*')
+		print($nb1 * $nb2 . "\n");
+	else if ($sign == '/')
+		print($nb1 / $nb2 . "\n");
+	else if ($sign == '%')
+		print($nb1 % $nb2 . "\n");
+}
+else
+	print("Incorrect Parameters\n");
+?>
