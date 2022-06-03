74f11d58bd47971bf53eafac4c18bc657f08c502
Henri <henrimaronen@gmail.com>
2022-06-03 09:29:56 +0300

    ssap2 done

--- day01/ex06/ssap.php
+++ day01/ex06/ssap.php
@@ -5,11 +5,11 @@ unset($argv[0]);
 $words= array();
 while ($i < $argc)
 {
-    $arr = explode(' ', $argv[$i]);
-    $words = array_merge($words, $arr);
-    $i++;
+	$arr = explode(' ', $argv[$i]);
+	$words = array_merge($words, $arr);
+	$i++;
 }
 sort($words);
 foreach($words as $word)
-    print("$word\n");
+	print("$word\n");
 ?>
--- day01/ex09/ssap2.php
+++ day01/ex09/ssap2.php
@@ -1,71 +1,32 @@
 #!/usr/bin/php
 <?php
-	// function sort_array($array, $b)
-	// {
-	// 	// $str = '!"#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-	// 	$str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	// 	$str2 = "abcdefghijklmnopqrstuvwxyz";
-	// 	$str3 = "0123456789";
-	// 	$i = 0;
-	function sort_array($a, $b)
-	{
-		$line = "abcdefghijklmnopqrstuvwxyz0123456789!\"";
-	}
-	function foo($str)
-	{
-		while ($word[$i] != 0)
+		function sort_array($str1, $str2)
 		{
-			// print("$word[$i]");
-			if (stripos($word[$i], $numbers))
+			$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+			$i = 0;
+			while ($str1[$i] || $str2[$i])
 			{
-				array_push($numbers, $word);
-				break;
+				$pos1 = stripos($chars, $str1[$i]);
+				$pos2 = stripos($chars, $str2[$i]);
+				if ($pos1 > $pos2)
+					return (1);
+				else if ($pos1 < $pos2)
+					return (-1);
+				else
+					return (0);
+				$i++;
 			}
-			else if (stripos($word[$i], $signs))
-				array_push($other, $word);
-			else
-				array_push($alpha, $word);
-			$i++;
 		}
-	}
+
 	unset($argv[0]);
 	$words = array();
-	$alpha = array();
-	$numerics = array();
-	$other = array();
-	$alphabet = "abcdefghijklmnopqrstuvwxyz";
-	$numbers = "0123456789";
-	$signs = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-	$i = 0;
-
 	foreach ($argv as $word)
 	{
 		$arr = explode(' ', $word);
 		$words = array_merge($words, $arr);
 	}
+
+	usort($words, "sort_array");
 	foreach ($words as $word)
-	{
 		print("$word\n");
-		foo($word);
-		// while ($word[$i] != 0)
-		// {
-		// 	// print("$word[$i]");
-		// 	if (stripos($word[$i], $numbers))
-		// 	{
-		// 		array_push($numbers, $word);
-		// 		break;
-		// 	}
-		// 	else if (stripos($word[$i], $signs))
-		// 		array_push($other, $word);
-		// 	else
-		// 		array_push($alpha, $word);
-		// 	$i++;
-		// }
-	}
-		// print_r($numbers);
-		// print_r($alpha);
-		// print_r($other);
-	//usort($words, "sort_array");
-	//foreach ($words as $word)
-	//	print("$word\n");
 ?>
