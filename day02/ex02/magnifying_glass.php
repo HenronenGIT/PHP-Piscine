2468990c5a9dcff7e3283f815f1c03cb054a9b2c
Henri <henrimaronen@gmail.com>
2022-06-08 18:59:50 +0300

    small fix

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -7,7 +7,7 @@
 		$result = preg_replace_callback("/(?<=<a) .*?>(.*?)</", function($match) {
 				return (str_replace($match[1], strtoupper($match[1]), $match[0]));
 			}, $file);
-			$result = preg_replace_callback("/(?<=title=\")(.*)</", function($match) {
+			$result = preg_replace_callback("/(?<=title=\")(.*?)</s", function($match) {
 			return (str_replace($match[1], strtoupper($match[1]), $match[0]));
 		}, $result);
 		print($result);
