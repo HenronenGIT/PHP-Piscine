b2b638c8afeded90eff5afacdb06c2871577e9be
Henri <henrimaronen@gmail.com>
2022-06-08 14:54:16 +0300

    ex02 ready

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -3,13 +3,13 @@
 	if ($argc == 2)
 	{
 		$file = file_get_contents($argv[1]);
-		// print($file);
-		$result = preg_replace_callback("/(?<=title=\")(.*)</", function($match) {
-			print($match[1] . "\n");
-			print($match[0] . "\n");
-			return (str_replace($match[1], strtoupper($match[1]), $match[0]));
-		}, $file);
 
+		$result = preg_replace_callback("/(?<=<a) .*?>(.*?)</", function($match) {
+				return (str_replace($match[1], strtoupper($match[1]), $match[0]));
+			}, $file);
+			$result = preg_replace_callback("/(?<=title=\")(.*)</", function($match) {
+			return (str_replace($match[1], strtoupper($match[1]), $match[0]));
+		}, $result);
 		print($result);
 	}
 ?>
