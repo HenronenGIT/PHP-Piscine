7a04f421b6db3f297d4d4533ee574691efca0228
Henri <henrimaronen@gmail.com>
2022-06-09 14:22:00 +0300

    Test files removed and now works with both files

--- day02/ex02/magnifying_glass.php
+++ day02/ex02/magnifying_glass.php
@@ -5,12 +5,12 @@
 	if ($argc == 2)
 	{
 		$str = implode("", file($argv[1]));
-		$result = $str;
+		$copy = $str;
 		preg_match_all($regex0, $str, $match);
 		$str = implode("", $match[0]);
 		preg_match_all($regex1, $str, $match);
 		foreach($match[0] as $string)
-			$result = preg_replace("/$string/", strtoupper($string), $result);
-		print($result);
+			$copy = preg_replace("/$string/", strtoupper($string), $copy);
+		print($copy);
 	}
 ?>
