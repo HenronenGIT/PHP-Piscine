990abd9aa2aea5c195861d1f22b8831237ec8e85
Henri <henrimaronen@gmail.com>
2022-06-09 12:00:49 +0300

    small syntax fixes

--- day05/ex06/ex06.sql
+++ day05/ex06/ex06.sql
@@ -1,5 +1,4 @@
 SELECT `title`, `summary`
 FROM `film`
-WHERE `summary`
-LIKE '%Vincent%'
+WHERE `summary` LIKE '%Vincent%'
 ORDER BY `id_film` ASC;
--- day05/ex20/ex20.sql
+++ day05/ex20/ex20.sql
@@ -1,9 +1,8 @@
-SELECT  film.id_genre,
-        genre.name AS 'name_genre',
-        dist.id_distrib AS 'id_distrib',
-        dist.name AS 'name_distrib',
-        film.title AS 'title_film'
-FROM `film`
-LEFT JOIN `genre` ON genre.id_genre = film.id_genre
-LEFT JOIN `distrib` AS `dist` ON dist.id_distrib = film.id_distrib
-WHERE film.id_genre BETWEEN '4' AND '8';
+SELECT	film.id_genre AS 'id_genre', 
+		genre.name AS 'name_genre', 
+		film.id_distrib AS 'id_distrib', 
+		distrib.name AS 'name_distrib', 
+		film.title AS 'title_film' FROM film
+LEFT JOIN genre ON genre.id_genre = film.id_genre
+LEFT JOIN distrib ON distrib.id_distrib = film.id_distrib
+WHERE film.id_genre BETWEEN 4 AND 8;
