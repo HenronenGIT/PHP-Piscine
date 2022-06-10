80d9ca67ca98b3be3e82fc7f7adfa19306a0d1a2
Henri <henrimaronen@gmail.com>
2022-06-10 15:56:58 +0300

    Cleaning and last polish

--- day06/ex01/Vertex.class.php
+++ day06/ex01/Vertex.class.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "Color.class.php";
+
 class Vertex{
 	public static $verbose = False;
 	private $_x = 0.0;
@@ -9,24 +10,22 @@ class Vertex{
 	private $_w = 1.0;
 	private $_color;
 
-	function	__construct($array)
-	{
+	function	__construct($array) {
 		$this->_x = $array['x'];
 		$this->_y = $array['y'];
 		$this->_z = $array['z'];
 		if ($array['w'])
-			$this->_w = $array['w'];
+		$this->_w = $array['w'];
 		if (!array_key_exists(color, $array))
-			$this->_color = new Color( array('red' => 255, 'green' => 255, 'blue' => 255));
+		$this->_color = new Color( array('red' => 255, 'green' => 255, 'blue' => 255));
 		else
-			$this->_color = $array['color'];
+		$this->_color = $array['color'];
 		if (self::$verbose)
 			print($this . " constructed\n");
 		return ;
 	}
 	
-	function	__toString()
-	{
+	function	__toString() {
 		if (self::$verbose)
 		{
 			$str = sprintf(	"Vertex( x: %3.2f, y: %3.2f, z:%3.2f, w:%3.2f, ", $this->_x, $this->_y, $this->_z, $this->_w);
@@ -37,24 +36,25 @@ class Vertex{
 		return ($str);
 	}
 
-	static function	doc()
-	{
+	static function	doc() {
 		return (file_get_contents("Vertex.doc.txt"));
 	}
 	
-	function	__destruct()
-	{
+	function	__destruct() {
 		if (self::$verbose)
 			print($this . " destructed\n");
 		return ;
 	}
-	public function getX() { return ($this->_x);}
+
+	public function getX() { return (($this->_x));}
 	public function getY() { return ($this->_y);}
 	public function getZ() { return ($this->_z);}
+	public function getW() { return ($this->_w);}
 	public function getColor() { return ($this->_color);}
 	public function setX($x) { $this->_x = $x;}
 	public function setY($y) { $this->_y = $y;}
 	public function setZ($z) { $this->_z = $z;}
+	public function setW($w) { $this->_w = $w;}
 	public function setColor($color) { $this->_color = $color;}
 }
 ?>
